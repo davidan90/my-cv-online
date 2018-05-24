64014b5afd241ba76195909ae623dda21f272c6a
David <david@MacBook-Air-de-David.local>
2018-05-25 00:25:49 +0200

    add start-backdrop

--- src/app.tsx
+++ src/app.tsx
@@ -3,14 +3,39 @@ import { HomeLayout, ContactLayout, SkillsLayout, CvLayout } from './layouts';
 import { AppHeader } from './components';
 import './app.scss';
 
-export const App = () => (
-    <div className='dan-app-container'>
-        <AppHeader />
-        <React.Fragment>
-            <ContactLayout />
-            <HomeLayout />
-            <SkillsLayout />
-            <CvLayout />
-        </React.Fragment>
-    </div>
-);
+export class App extends React.Component<{}, {start: boolean}> {
+
+    constructor(props: {}) {
+        super(props);
+        this.state = {
+            start: false
+        };
+    }
+
+    renderApp = (start: boolean) => start ? (
+        <div className='dan-app-container'>
+            <AppHeader />
+            <React.Fragment>
+                <ContactLayout />
+                <HomeLayout />
+                <SkillsLayout />
+                <CvLayout />
+            </React.Fragment>
+        </div>
+    ) : null
+
+    render() {
+        const { start } = this.state;
+        return (
+            <React.Fragment>
+                <a
+                    className={`start-backdrop${start ? ' hide' : ''}`}
+                    href='#home'
+                    onClick={() => this.setState({start: true})}>
+                    Comenzar
+                </a>
+                {this.renderApp(start)}
+            </React.Fragment>
+        );
+    }
+}
